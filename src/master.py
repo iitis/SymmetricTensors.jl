8fe491a885cd336bd822dc2ca5b65f3446e3ba5c
Krzysztof Domino <kdomino@iitis.pl>
2016-01-19 15:30:06 +0100

    parameter flow

--- src/master.py
+++ src/master.py
@@ -20,14 +20,15 @@ from scipy import io
 
 # ----------------------------------------------------------------------------
 
-def reduce_X_dimension(X):
+def reduce_X_dimension(X, l, u):
     """Reduce number of spectral features."""
-    return X[:,70:130]
+    return X[:,l:u]
 
 # ----------------------------------------------------------------------------
 
 class TransformNone(object):
-    """Nothing"""
+    def __str__(self):
+        return "Nothing"
     
     def fit(self, X):
         pass
@@ -38,7 +39,11 @@ class TransformNone(object):
 # ----------------------------------------------------------------------------
 
 class TransformPCA(object):
-    """PCA n_c=5"""
+    def __init__(self, k2):
+        self.k2 = k2
+
+    def __str__(self):
+        return "PCA, n_c = {}".format( self.k2)
     
     def fit(self, X):
         self.pca = RandomizedPCA(5).fit(X)
@@ -49,19 +54,24 @@ class TransformPCA(object):
 # ----------------------------------------------------------------------------
 
 class TransformCumulants(object):
-    """Cumulants k_3 = 2, k_4 = 2, n_c = 5"""
+
+    def __init__(self, k2, k3, k4):
+        self.k2 = k2
+        self.k3 = k3
+        self.k4 = k4
+
+    def __str__(self):
+        return "Cumulants k_3 = {}, k_4 = {}, n_c = {}".format(self.k3, self.k4, self.k2)
 
     def fit(self, X):
         np.save('test.npy', X)
 
-        def run_jm(dir):
+        def run_jm(dir, k3, k4):
             import subprocess
             import matlab.engine
             subprocess.call(["julia", "get_cu.jl"])
             eng = matlab.engine.start_matlab()
             eng.addpath(dir)
-            k3=2
-            k4=2
             U3, U4 = eng.lrtd(k3, k4, nargout=2)
             return np.mat(U3), np.mat(U4)
         def pca(x, n):
@@ -72,12 +82,10 @@ class TransformCumulants(object):
             return np.asmatrix(U[:, sorted_indices_desc])
 
 
-
-
         directory = '/home/krzysztof/Dokumenty/badania_iitis/tensors_symetric/tensor calculations/pictures_tensor/'
-        self.U3, self.U4 = run_jm(directory)
+        self.U3, self.U4 = run_jm(directory,self.k3,self.k4)
         #self.pca = RandomizedPCA(4).fit(X)
-        self.U2 = pca(X,5)
+        self.U2 = pca(X,self.k2)
 
     def transform(self, X):
         features_v = np.hstack([X*self.U2, X*self.U3,X*self.U4])
@@ -93,8 +101,9 @@ if __name__ == '__main__':
     hsdata = load_indian_pines('low_rank_tensor_approx/m_hsbase/data/')
     hsdata = prepare_hsdata(hsdata)
     X, y, idx = hsdata['X'], hsdata['y'], hsdata['train1all']
-    
-    X = reduce_X_dimension(X) # if needed, reduce X dimension
+    l = 70
+    u = 130
+    X = reduce_X_dimension(X, l, u) # if needed, reduce X dimension
 
     # display selection of training data and ground truth
     plt.figure()
@@ -103,11 +112,15 @@ if __name__ == '__main__':
     plt.figure()
     plt.imshow(hsdata['truth'], interpolation='nearest', cmap=plt.cm.spectral)
     plt.gcf().canvas.set_window_title('Ground truth')
+    k2_pca = 10
+    k2 = 10
+    k3 = 2
+    k4 = 2
 
-    pp = PdfPages('par_5_2_2_5_dat70_130.pdf') #KD added line
+    pp = PdfPages('par_'+str(k2_pca)+'_'+str(k4)+'_'+str(k3)+'_'+str(k2)+'_dat'+str(l)+'_'+str(u)+'.pdf') #KD added line
     # test several transforms
     n_neighbors = 3
-    for tt in [TransformNone(), TransformPCA(), TransformCumulants()]:
+    for tt in [TransformNone(), TransformPCA(k2_pca), TransformCumulants(k2,k3,k4)]:
         knn = KNeighborsClassifier(n_neighbors=n_neighbors)
         X_train, y_train = X[idx], y[idx]
         tt.fit(X_train) #zachowuje U1 U2 U3 - stworzyl je
@@ -121,7 +134,7 @@ if __name__ == '__main__':
         plot = plt.figure()
         visualize_classes(hsdata)
         info = 'Classifier (KNN, n={})'.format(n_neighbors)
-        info += ', transform={}'.format(tt.__doc__)
+        info += ', transform={}'.format(str(tt))
         plt.gcf().canvas.set_window_title(info)
         plt.text(0,157,str(info), fontsize = 10) #KD added line
         pp.savefig(plot) #KD added line
