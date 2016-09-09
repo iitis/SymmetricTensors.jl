f56549117f8216a68c2b118b19c32e67211fdd21
Krzysztof Domino <kdomino@ad.iitis.pl>
2016-09-09 13:46:44 +0200

    poprawiony kod

--- src/cumulantsbs.jl
+++ src/cumulantsbs.jl
@@ -25,7 +25,7 @@ input vectors that corresponds to given column of data
 
 Returns Float64 (an element of the block)
 """
-momentel{T <: AbstractFloat}(v::Vector{T}...) = mean(mapreduce(i -> v[i], .*, 1:size(v,1)))
+momentel{T <: AbstractFloat}(v::Vector{T}...) = mean(mapreduce(i -> v[i], .*, 1:length(v)))
 
 """calculate n'th moment for the given segment
 
@@ -33,37 +33,68 @@ input r - matrices of data
 
 Returns N dimentional array (segment)
 """
-
-function momentseg{T <: AbstractFloat}(r::Matrix{T}...)
-  N = size(r, 1)
-  dims = map(i -> size(r[i], 2), 1:N)
+function momentseg{T <: AbstractFloat}(dims::Array{Int}, Y::Matrix{T}...)
+  n = length(Y)
 #  ret = SharedArray(T, dims...)
   ret = zeros(T, dims...)
 #  @sync @parallel
   for i = 1:prod(dims)
     ind = ind2sub((dims...), i)
-    @inbounds ret[ind...] = momentel(map(k -> r[k][:,ind[k]], 1:N)...)
+    @inbounds ret[ind...] = momentel(map(k -> Y[k][:,ind[k]], 1:n)...)
   end
   ret
 end
+
+"""axilaiary function that creates indices for blocks
+"""
+sqseg(i::Int, of::Int) =  (i-1)*of+1 : i*of
+
 
 """ calculate N'th moment in the bs form
 
 input matrix of data, the order of the moment (N), number of segments for bs
 
 Returns N dimentional Box structure of N'th moment
+
+Particular case if all boxes are squared
 """
-function momentbs{T <: AbstractFloat}(m::Matrix{T}, N::Int, segments::Int = 2)
-    len = size(m,2)
-    segsizetest(len, segments)
-    (len%segments == 0)? () : segments += 1
-    ret = NullableArray(Array{T, N}, fill(segments, N)...)
-    segsize = ceil(Int, len/segments)
-    ind = indices(N, segments)
-    for i in ind
-      @inbounds ret[i...] = momentseg(map(k -> m[:,seg(i[k], segsize, len)], 1:N)...)
+function centrmom{T <: AbstractFloat}(X::Matrix{T}, n::Int, s::Int)
+    g = div(size(X,2), s)
+    ret = NullableArray(Array{T, n}, fill(g, n)...)
+    dims = fill(s, n)
+    for i in indices(n, g)
+      @inbounds ret[i...] = momentseg(dims, map(k -> X[:,sqseg(i[k], s)], 1:n)...)
     end
     BoxStructure(ret)
+end
+
+
+""" calculate N'th moment in the bs form
+
+input matrix of data, the order of the moment (N), number of segments for bs
+
+Returns N dimentional Box structure of N'th moment
+
+Case if last boxes are not squared
+"""
+function centrmomnsq{T <: AbstractFloat}(X::Matrix{T}, n::Int, s::Int)
+    M = size(X,2)
+    g = ceil(Int, M/s)
+    ret = NullableArray(Array{T, n}, fill(g, n)...)
+    for i in indices(n, g)
+      Y = map(k -> X[:,seg(i[k], s, M)], 1:n)
+      dims = map(i -> (size(Y[i], 2)), 1:n)
+      @inbounds ret[i...] = momentseg(dims, Y...)
+    end
+    BoxStructure(ret)
+end
+
+"""examines if the last box is square or not
+
+and call the proper function
+"""
+function momentbs{T <: AbstractFloat}(X::Matrix{T}, n::Int, s::Int = 2)
+    (size(X,2)%s == 0)? centrmom(X,n,s) : centrmomnsq(X,n,s)
 end
 
 #--- following code is used to calculate cumulants ----
@@ -85,33 +116,34 @@ c - arrays of boxes
 
 Returns N dimentional array of size s x .... x s
 """
-function productseg{T <: AbstractFloat}(s::Int, N::Int, part::Vector{Vector{Int}}, c::Array{T}...)
-  ret = zeros(T, fill(s, N)...)
-  for i = 1:(s^N)
-    ind = ind2sub((fill(s, N)...), i)
+function prodblocks{T <: AbstractFloat}(s::Int, n::Int, part::Vector{Vector{Int}}, c::Array{T}...)
+  ret = zeros(T, fill(s, n)...)
+  for i = 1:(s^n)
+    ind = ind2sub((fill(s, n)...), i)
     pe = splitind(collect(ind), part)
     @inbounds ret[ind...] = mapreduce(i -> c[i][pe[i]...], *, 1:size(part, 1))
+    #size(c, 1) = size(part, 1)
   end
   ret
 end
 
-#todo opis
-""" determines all permutations of st of sequence of intigeers into given number of subsets
-such permutations will be used to split multiindex into subsets
-and which element of the bs list correspond to such permutation
+
+""" given to multiindex lenght n and number of subsets omega
+provides all partitions of the sequence 1:n into sigma subsets such that each
+subset size is greater than 1 and subsets are disjoint
 """
 
 function indpart(n::Int, sigma::Int)
-    perm = Vector{Vector{Int}}[]
-    c_order = Vector{Int}[]
-    for p in partitions(1:n, sigma)
-      s = map(length, p)
+    p = Vector{Vector{Int}}[]
+    r = Vector{Int}[]
+    for part in partitions(1:n, sigma)
+      s = map(length, part)
       if !(1 in s)
-        push!(perm, p)
-        push!(c_order, s)
+        push!(p, part)
+        push!(r, s)
       end
     end
-    perm, c_order, size(c_order, 1)
+    p, r, length(r)
 end
 
 
@@ -131,35 +163,64 @@ function addzeros{T <: AbstractFloat, N}(s::Int, inputbox::Array{T,N})
   inputbox
 end
 
-"""checks if all bloks in bs are squred and call the proper function that calculates mixed elements for the n'th cumulant
+"""
+calculates mixed element for given sigma, if all blockes are squared
 
-input part - a vector of partitions, bscum - cumulants of order 2 - (n-2) in bs form in the following order c2, c3, ..., c(n-2)
+input n - order of product, sigma - number of subsets,
+c2, c3, ..., c(n-2) - lowe cumulants input
 
 Returns the porper outer product function
 """
-
-function ccomposition{T <: AbstractFloat}(N::Int, sigma::Int, bscum::BoxStructure{T}...)
-  s = size(bscum[1])
-  issquare = (s[1]*s[2] == s[3])
-  p, part, len = indpart(N, sigma)
-  ret = NullableArray(Array{T, N}, fill(s[2], N)...)
-  for i in indices(N, s[2])
-    temp = zeros(T, fill(s[1], N)...)
+function outerprod{T <: AbstractFloat}(n::Int, sigma::Int, c::BoxStructure{T}...)
+  s,g,M = size(c[1])
+  p, r, len = indpart(n, sigma)
+  ret = NullableArray(Array{T, n}, fill(g, n)...)
+  for i in indices(n, g)
+    temp = zeros(T, fill(s, n)...)
     for j in 1:len
       pe = splitind([i...], p[j])
-      if issquare || !(s[2] in i)
-        @inbounds temp += productseg(s[1], N, p[j], map(i -> bscum[part[j][i]-1].frame[pe[i]...].value, 1:sigma)...)
+      @inbounds temp += prodblocks(s, n, p[j], map(l -> c[r[j][l]-1].frame[pe[l]...].value, 1:sigma)...)
+    end
+    @inbounds ret[i...] = temp
+  end
+  BoxStructure(ret)
+end
+
+"""
+calculates mixed element for given sigma, if last blockes are not squared
+
+"""
+function outerprodnsq{T <: AbstractFloat}(n::Int, sigma::Int, c::BoxStructure{T}...)
+  s,g,M = size(c[1])
+  p, r, len = indpart(n, sigma)
+  ret = NullableArray(Array{T, n}, fill(g, n)...)
+  for i in indices(n, g)
+    temp = zeros(T, fill(s, n)...)
+    for j in 1:len
+      pe = splitind([i...], p[j])
+      if (g in i)
+        @inbounds temp += prodblocks(s, n, p[j], map(l -> addzeros(s[1], c[r[j][l]-1].frame[pe[l]...].value), 1:sigma)...)
       else
-        @inbounds temp += productseg(s[1], N, p[j], map(i -> addzeros(s[1], bscum[part[j][i]-1].frame[pe[i]...].value), 1:sigma)...)
+        @inbounds temp += prodblocks(s, n, p[j], map(l -> c[r[j][l]-1].frame[pe[l]...].value, 1:sigma)...)
       end
     end
-    if !(issquare || !(s[2] in i))
-      range = map(k -> ((s[2] == i[k])? (1:(s[3]%s[1])) : (1:s[1])), 1:size(i,1))
+    if (g in i)
+      range = map(k -> ((g == i[k])? (1:(M%s)) : (1:s)), 1:length(i))
       temp = temp[range...]
     end
     @inbounds ret[i...] = temp
   end
   BoxStructure(ret)
+end
+
+
+"""checks if all bloks in are squred and returns
+
+the proper function that calculates mixed elements for the n'th cumulant
+"""
+function outerp{T <: AbstractFloat}(n::Int, sigma::Int, c::BoxStructure{T}...)
+  s,g,M = size(c[1])
+  (M%s == 0)? outerprod(n,sigma,c...) : outerprodnsq(n,sigma,c...)
 end
 
 """calculates n'th cumulant,
@@ -168,10 +229,10 @@ input data - matrix of data, n - the order of the cumulant, segments - number of
 c - cumulants in the bs form orderred as follow c2, c3, ..., c(n-2)
 
 Returns the n order cumulant in the bs form"""
-function cumulantn{T <: AbstractFloat}(data::Matrix{T}, n::Int, segments::Int, c::BoxStructure{T}...)
-  ret = momentbs(data, n, segments)
+function cumulantn{T <: AbstractFloat}(X::Matrix{T}, n::Int, s::Int, c::BoxStructure{T}...)
+  ret =  momentbs(X, n, s)
   for sigma in 2:floor(Int, n/2)
-    ret -= ccomposition(n, sigma, c...)
+    ret -= outerp(n, sigma, c...)
   end
   ret
 end
@@ -185,15 +246,11 @@ Returns cumulants in the bs form orderred as follow c2, c3, ..., cn
 works for any n >= 2, tested up to n = 10, in automatic tests up to n = 6 (limit due to the increasement
 in computation time for benchmark algorithm (semi naive))
 """
-function cumulants{T <: AbstractFloat}(n::Int, data::Matrix{T}, segments::Int = 2)
-  data = centre(data)
-  ret = Array(BoxStructure, n-1)
+function cumulants{T <: AbstractFloat}(n::Int, X::Matrix{T}, s::Int = 3)
+  centre!(X)
+  ret = Array(BoxStructure{T}, n-1)
   for i = 2:n
-    if i < 4
-      ret[i-1] = momentbs(data, i, segments)
-    else
-      ret[i-1] = cumulantn(data, i, segments, ret[1:(i-3)]...)
-    end
+    ret[i-1] =  (i < 4)? momentbs(X, i, s) : cumulantn(X, i, s, ret[1:(i-3)]...)
   end
   ret
 end
